b434916eaf167268c9f57904a7f813f4c1c19674
Ahmed Mohamed <ahmedabozied699@gmail.com>
2023-05-22 14:12:45 +0300

    Updates

---
+++ precache-manifest.7aa9558d7779159cd1143ab7e0c351b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8199ff72f3bcfbc90a3443181fcc499",
+    "url": "/Image-Editing/index.html"
+  },
+  {
+    "revision": "1f0f9401987a4c5ab1f3",
+    "url": "/Image-Editing/static/css/main.e901bb20.chunk.css"
+  },
+  {
+    "revision": "b8def29ec7a2307fe853",
+    "url": "/Image-Editing/static/js/2.19cffbbe.chunk.js"
+  },
+  {
+    "revision": "a488a4f8f41d5d705b3dced8f63e5340",
+    "url": "/Image-Editing/static/js/2.19cffbbe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f0f9401987a4c5ab1f3",
+    "url": "/Image-Editing/static/js/main.a9b307d1.chunk.js"
+  },
+  {
+    "revision": "5935607e64942f2f63e2",
+    "url": "/Image-Editing/static/js/runtime-main.df93b003.js"
+  }
+]);
